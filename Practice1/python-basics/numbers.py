a3adce62e77f13e05d1c6483ee1960a6085d3967
Ulan <suanulan@gmail.com>
2026-01-27 17:21:40 +0500

    add new codes practice 1

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,7 @@
+x = 5689
+y = 5468.656
+z = 456j
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,9 @@
+a = "Hello"
+z = "World!"
+
+print (a , z)
+
+t = "Python"
+g = " is fantastic"
+
+print (t + g)
